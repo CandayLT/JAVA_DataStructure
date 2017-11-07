8a6d88726745b9c8976f49f294b9847fc7ae824b
TangChen <1210208878@qq.com>
2017-11-07 22:51:39 +0800

    用数据结构实现的邻接矩阵表示的图与深度优先便利（DFS）

---
+++ src/Graph/DFS.java
@@ -0,0 +1,33 @@
+package Graph;
+
+import java.util.*;
+
+/**
+ * Created by TangChen on 17/11/7.
+ */
+public class DFS {
+    Graph graph;
+    Map<Character, Boolean> isVisit = new HashMap<>();
+    List<Character> DFSList = new ArrayList<>();
+
+    public DFS(Graph graph) {
+        this.graph = graph;
+
+        for (int i = 0; i < graph.graphNum; i++) {
+            isVisit.put(graph.vertexElement[i], false);
+        }
+    }
+
+    public void toDFS(char c) {
+        if (!isVisit.get(c)) {
+            System.out.print(c + " -> ");
+            isVisit.put(c, true);
+
+            for (int i = 0; i < graph.graphNum; i++) {
+                if (graph.matrix[graph.locElement(c)][i] == 1 && !isVisit.get(graph.vertexElement[i])) {
+                    toDFS(graph.vertexElement[i]);
+                }
+            }
+        }
+    }
+}
